f6c4ccda5350e6f8ba0c12f00df603fc5efc8936
张亚飞 <672233580@qq.com>
2021-06-25 12:54:30 +0800

    Initial Commit

---
+++ ThreeSquareLoader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ThreeSquareLoader
+//
+//  Created by 张亚飞 on 2021/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThreeSquareLoader/ThreeSquareLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreeSquareLoaderApp.swift
+//  ThreeSquareLoader
+//
+//  Created by 张亚飞 on 2021/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ThreeSquareLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
